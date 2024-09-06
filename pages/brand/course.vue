8476a4fce0a74ed65ce6e760d82c21273bb5f5a1
qiwu96 <zbb1064212882@126.com>
2024-09-06 14:55:08 +0800

    fix: 更新發展歷程

--- pages/brand/course.vue
+++ pages/brand/course.vue
@@ -1,11 +1,21 @@
 <script lang="ts" setup>
 import { useAppState } from '~/stores/appState'
 import { ref } from 'vue'
-import { Controller } from 'swiper'
+import { Controller, Scrollbar } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/vue'
 const appState = useAppState()
 appState.setBrand('course')
 
+let swiperRef = {
+  slidePrev: () => {},
+  slideNext: () => {},
+}
+const setSwiperRef = (swiper: any) => {
+  swiperRef = swiper
+}
+const handleProcessBtn = (_type: string) => {
+  swiperRef[_type]()
+}
 useHead({
   title: '品牌介紹 | 口腔專科醫院',
   meta: [
@@ -533,6 +543,229 @@ onMounted(() => {
           />
         </div>
       </div>
+      <div class="new_course_box">
+        <div class="dentistryServices-title reason-title">
+          <div class="dentistryServices-title-in bb reason-title-in">
+            最新活動
+          </div>
+        </div>
+        <BrandCourse />
+      </div>
+      <div class="honor-box">
+        <!-- 荣誉 -->
+        <div class="honor">
+          <div class="honor-bg">
+            <div class="dentistryServices-title reason-title">
+              <div class="dentistryServices-title-in bb reason-title-in">
+                品牌榮譽
+              </div>
+            </div>
+            <div class="honor-new_text">
+              <p>CKJ愛康健口腔品牌連鎖</p>
+              <p>專注口腔，屢獲殊榮</p>
+            </div>
+            <div class="honor-title">
+              <span>{{
+                $t('pages.brand.honor.honorSwiper.title.span_1')
+              }}</span>
+              <span>{{
+                $t('pages.brand.honor.honorSwiper.title.span_2')
+              }}</span>
+            </div>
+            <div class="honor-in smallPageCon">
+              <div class="leftBtn" @click="handleProcessBtn('slidePrev')"></div>
+              <swiper
+                slidesPerView="auto"
+                :loop="true"
+                class="honorSwiper"
+                @swiper="setSwiperRef"
+              >
+                <swiper-slide class="swiper-slide">
+                  <img
+                    loading="lazy"
+                    src="https://static.cmereye.com/imgs/2023/06/154e36113fa5e579.png"
+                    alt=""
+                  />
+                </swiper-slide>
+                <swiper-slide class="swiper-slide">
+                  <img
+                    loading="lazy"
+                    src="https://static.cmereye.com/imgs/2023/06/ac0aa9264119857f.png"
+                    alt=""
+                  />
+                </swiper-slide>
+              </swiper>
+              <div
+                class="rightBtn"
+                @click="handleProcessBtn('slideNext')"
+              ></div>
+            </div>
+          </div>
+        </div>
+        <!-- 国际 -->
+        <div class="international">
+          <div class="international-title">
+            <span>{{
+              $t('pages.brand.honor.international.title.span_1')
+            }}</span>
+            <span>{{
+              $t('pages.brand.honor.international.title.span_2')
+            }}</span>
+          </div>
+          <div class="international-context">
+            {{ $t('pages.brand.honor.international.context.item_1') }}<br />
+            {{ $t('pages.brand.honor.international.context.item_2') }}<br />
+            {{ $t('pages.brand.honor.international.context.item_3') }}<br />
+            <br />
+            {{ $t('pages.brand.honor.international.context.item_4') }}<br />
+            {{ $t('pages.brand.honor.international.context.item_5') }}<br />
+            {{ $t('pages.brand.honor.international.context.item_6') }}<br />
+            {{ $t('pages.brand.honor.international.context.item_7') }}<br />
+          </div>
+          <div class="international-in smallPageCon">
+            <!-- https://static.cmereye.com/imgs/2023/05/f358efedd13763fe.jpg -->
+            <swiper
+              slidesPerView="auto"
+              :scrollbar="{
+                hide: true,
+              }"
+              :modules="[Scrollbar]"
+              class="internationalSwiper"
+            >
+              <swiper-slide class="swiper-slide">
+                <img
+                  loading="lazy"
+                  src="https://static.cmereye.com/imgs/2023/05/077256e4a998aad2.jpg"
+                  alt=""
+                />
+              </swiper-slide>
+              <swiper-slide class="swiper-slide">
+                <img
+                  loading="lazy"
+                  src="https://static.cmereye.com/imgs/2023/05/d528ef75d3159deb.jpg"
+                  alt=""
+                />
+              </swiper-slide>
+              <swiper-slide class="swiper-slide">
+                <img
+                  loading="lazy"
+                  src="https://static.cmereye.com/imgs/2023/05/bd7fb4779d941c03.jpg"
+                  alt=""
+                />
+              </swiper-slide>
+            </swiper>
+          </div>
+        </div>
+        <!-- 品牌 -->
+        <div class="brand">
+          <div class="brand-title">
+            <div class="brand-title-in">
+              <span>{{ $t('pages.brand.honor.brand_item.title.span_1') }}</span>
+              <span>{{ $t('pages.brand.honor.brand_item.title.span_2') }}</span>
+            </div>
+          </div>
+          <div class="brand-context">
+            {{ $t('pages.brand.honor.brand_item.context') }}
+          </div>
+          <div class="brand-in">
+            <div>
+              <div>
+                <img
+                  loading="lazy"
+                  src="https://static.cmereye.com/imgs/2024/07/9c91a9fd48c2180b.png"
+                  alt=""
+                />
+              </div>
+              <div>
+                <span>{{ $t('pages.brand.honor.brand_item.in_1') }}</span>
+              </div>
+            </div>
+            <div>
+              <div>
+                <img
+                  loading="lazy"
+                  src="https://static.cmereye.com/imgs/2024/07/922c7507efcd0304.png"
+                  alt=""
+                />
+              </div>
+              <div>
+                <span>{{ $t('pages.brand.honor.brand_item.in_2') }}</span>
+              </div>
+            </div>
+            <div>
+              <div>
+                <img
+                  loading="lazy"
+                  src="https://static.cmereye.com/imgs/2024/07/5b76646f4c37c695.png"
+                  alt=""
+                />
+              </div>
+              <div>
+                <span>{{
+                  $t('pages.brand.honor.brand_item.in_3.span_1')
+                }}</span>
+                <span>{{
+                  $t('pages.brand.honor.brand_item.in_3.span_2')
+                }}</span>
+                <span>{{
+                  $t('pages.brand.honor.brand_item.in_3.span_3')
+                }}</span>
+              </div>
+            </div>
+            <div>
+              <div>
+                <img
+                  loading="lazy"
+                  src="https://static.cmereye.com/imgs/2024/07/c68bc087e539b6d1.png"
+                  alt=""
+                />
+              </div>
+              <div>
+                <span>{{
+                  $t('pages.brand.honor.brand_item.in_4.span_1')
+                }}</span>
+                <span>{{
+                  $t('pages.brand.honor.brand_item.in_4.span_2')
+                }}</span>
+              </div>
+            </div>
+            <a
+              href="https://www.gma-awards.hk01.group/%E5%BE%97%E7%8D%8E%E5%90%8D%E5%96%AE"
+              target="_blank"
+            >
+              <div>
+                <img
+                  loading="lazy"
+                  src="https://static.cmereye.com/imgs/2024/07/017d06980d9fe3b3.png"
+                  alt=""
+                />
+              </div>
+              <div>
+                <span>香港01</span>
+                <span>傑出大灣區牙科醫療</span>
+                <span>服務機構</span>
+              </div>
+            </a>
+            <a
+              href="https://iet2.hket.com/event/hket_excellence_awards/zh/2024-winner-list.html"
+              target="_blank"
+            >
+              <div>
+                <img
+                  loading="lazy"
+                  src="https://static.cmereye.com/imgs/2024/07/44b305dac44d3274.png"
+                  alt=""
+                />
+              </div>
+              <div>
+                <span>香港經濟日報</span>
+                <span>傑出跨境牙科</span>
+                <span>醫療服務</span>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
       <NewAddress />
       <ContactForm-new />
     </div>
@@ -763,6 +996,9 @@ onMounted(() => {
     margin-bottom: 10px;
   }
 }
+.new_course_box {
+  margin: 125px auto;
+}
 .detail {
   display: flex;
   gap: 0 20px;
@@ -895,6 +1131,243 @@ onMounted(() => {
     }
   }
 }
+.honor-box {
+  margin-top: 125px;
+  margin-bottom: 125px;
+}
+.honor {
+  margin-top: 20px;
+  &-bg {
+    padding: 70px 0 106px;
+    background: linear-gradient(
+      270deg,
+      rgba(255, 241, 240, 0) 2.6%,
+      rgba(255, 241, 240, 0.7) 23.89%,
+      rgba(255, 241, 240, 0.7) 75.33%,
+      rgba(255, 241, 240, 0) 97.4%
+    );
+  }
+  &-new_text {
+    color: var(--Black, #4d4d4d);
+    text-align: center;
+    font-family: FakePearl;
+    font-size: 28px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 160%; /* 44.8px */
+    margin: 50px auto 80px;
+  }
+  &-title {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    span {
+      display: inline-block;
+      font-style: normal;
+      font-weight: 700;
+      font-size: 30px;
+      line-height: 160%;
+      // color: #666666;
+      color: var(--indexColor1);
+      // &:last-child {
+      //   border-bottom: 4px solid var(--indexColor1);
+      // }
+    }
+  }
+  &-in {
+    margin-top: 72px;
+    display: flex;
+    .leftBtn {
+      width: 70px;
+      background: var(--indexColor2);
+      cursor: pointer;
+      position: relative;
+      &::after {
+        content: '';
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-75%, -50%);
+        border-top: 20px solid;
+        border-right: 30px solid;
+        border-bottom: 20px solid;
+        border-left: 30px solid;
+        border-color: transparent #fff transparent transparent;
+        filter: drop-shadow(0px 0px 7.21491px rgba(255, 120, 117, 0.25));
+      }
+    }
+    .rightBtn {
+      width: 70px;
+      background: var(--indexColor);
+      cursor: pointer;
+      position: relative;
+      &::after {
+        content: '';
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-25%, -50%);
+        border-top: 20px solid;
+        border-right: 30px solid;
+        border-bottom: 20px solid;
+        border-left: 30px solid;
+        border-color: transparent transparent transparent #fff;
+        filter: drop-shadow(0px 0px 7.21491px rgba(255, 120, 117, 0.25));
+      }
+    }
+    .honorSwiper {
+      width: calc(100% - 140px);
+      overflow: hidden;
+      margin: 0;
+      .swiper-slide {
+        img {
+          width: 100%;
+        }
+      }
+    }
+  }
+}
+.international {
+  margin-top: 60px;
+  &-title {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    span {
+      display: inline-block;
+      font-style: normal;
+      font-weight: 700;
+      font-size: 30px;
+      line-height: 160%;
+      color: #666666;
+      &:last-child {
+        border-bottom: 4px solid var(--indexColor1);
+      }
+    }
+  }
+  &-context {
+    text-align: center;
+    margin-top: 44px;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 160%;
+    color: #666666;
+  }
+  &-in {
+    margin-top: 58px;
+    .internationalSwiper {
+      padding-bottom: 44px;
+      .swiper-slide {
+        padding: 0 15px;
+        &:nth-of-type(1) {
+          width: 105%;
+        }
+        &:nth-of-type(2) {
+          width: 91%;
+        }
+        &:nth-of-type(3) {
+          width: 131.5%;
+        }
+      }
+      :deep(.swiper-scrollbar) {
+        opacity: 1 !important;
+        width: 450px;
+        margin: 0 auto;
+        background: var(--indexColor2);
+        left: 50%;
+        transform: translateX(-50%);
+      }
+      :deep(.swiper-scrollbar-drag) {
+        background: var(--indexColor1);
+      }
+    }
+  }
+}
+.brand {
+  margin-top: 80px;
+  &-title {
+    font-style: normal;
+    font-weight: 700;
+    font-size: 26px;
+    line-height: 160%;
+    color: #666666;
+    display: flex;
+    justify-content: center;
+    &-in {
+      display: inline-block;
+      border-bottom: 4px solid var(--indexColor1);
+      span {
+        text-align: center;
+        display: block;
+      }
+    }
+  }
+  &-context {
+    font-style: normal;
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 160%;
+    text-align: center;
+    color: var(--indexColor1);
+    margin-top: 28px;
+  }
+  &-in {
+    width: 100%;
+    max-width: 1480px;
+    opacity: 0.8;
+    background: linear-gradient(
+      270deg,
+      rgba(255, 241, 240, 0) 2.6%,
+      rgba(255, 241, 240, 0.7) 23.89%,
+      rgba(255, 241, 240, 0.7) 75.33%,
+      rgba(255, 241, 240, 0) 97.4%
+    );
+    padding: 51px 0;
+    margin: 61px auto 0;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 70px 0px;
+    justify-content: space-between;
+    & > div,
+    & > a {
+      // flex: 1;
+      padding: 0 15px;
+      display: flex;
+      align-items: center;
+      & > div {
+        font-style: normal;
+        font-weight: 700;
+        font-size: 20px;
+        line-height: 160%;
+        text-align: center;
+        color: #4c4c4c;
+        white-space: pre-wrap;
+        &:last-child {
+          padding: 0 5px;
+        }
+        img {
+          // margin: 0 auto;
+          // max-width: 120px;
+        }
+      }
+      & > div:nth-child(2) {
+        color: var(--Grey-Deep, #4d4d4d);
+        font-family: FakePearl;
+        font-size: 26px;
+        font-style: normal;
+        font-weight: 500;
+        line-height: 160%; /* 41.6px */
+        letter-spacing: 2.6px;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+      }
+    }
+  }
+}
 @media (min-width: 1451px) and (max-width: 1920px) {
   .sidebar_1 {
     position: absolute;
@@ -978,6 +1451,14 @@ onMounted(() => {
       width: 100%;
     }
   }
+  .brand {
+    &-in {
+      & > div,
+      & > a {
+        padding: 0 2vw;
+      }
+    }
+  }
 }
 
 //md
@@ -1003,6 +1484,9 @@ onMounted(() => {
       font-weight: 600;
       line-height: 180%; /* 32px */
     }
+  }
+  .new_course_box {
+    margin: 60px auto 75px;
   }
   .detail {
     display: flex;
@@ -1270,6 +1754,154 @@ onMounted(() => {
         }
       }
     }
+  }
+  .honor {
+    margin-top: 30px;
+    &-bg {
+      padding: 44px 30px;
+      background: linear-gradient(
+        360deg,
+        rgba(255, 241, 240, 0) 0%,
+        rgba(255, 241, 240, 0.3) 12.5%,
+        rgba(255, 241, 240, 0.3) 81.99%,
+        rgba(255, 241, 240, 0) 100%
+      );
+    }
+    &-title {
+      span {
+        font-size: 26px;
+        &:last-child {
+          border-bottom: 2.5px solid var(--indexColor1);
+        }
+      }
+    }
+    &-in {
+      margin-top: 35px;
+      .leftBtn {
+        width: 15.5px;
+        &::after {
+          border-top: 5px solid;
+          border-right: 7px solid;
+          border-bottom: 5px solid;
+          border-left: 7px solid;
+          border-color: transparent #fff transparent transparent;
+        }
+      }
+      .rightBtn {
+        width: 15.5px;
+        &::after {
+          border-top: 5px solid;
+          border-right: 7px solid;
+          border-bottom: 5px solid;
+          border-left: 7px solid;
+          border-color: transparent transparent transparent #fff;
+        }
+      }
+      .honorSwiper {
+        width: calc(100% - 31px);
+      }
+    }
+  }
+  .international {
+    margin-top: 30px;
+    &-title {
+      span {
+        font-size: 26px;
+        &:last-child {
+          border-bottom: 2.5px solid var(--indexColor1);
+        }
+      }
+    }
+    &-context {
+      font-weight: 500;
+      font-size: 14px;
+      margin-top: 28px;
+    }
+    &-in {
+      margin-top: 28px;
+      .internationalSwiper {
+        padding-bottom: 30px;
+        .swiper-slide {
+          padding: 0 5px;
+          &:nth-of-type(1) {
+            margin-left: 25px;
+          }
+        }
+        :deep(.swiper-scrollbar) {
+          width: 249px;
+        }
+      }
+    }
+  }
+  .brand {
+    margin-top: 50px;
+    &-title {
+      font-size: 26px;
+      &-in {
+        border-bottom: 2.5px solid var(--indexColor1);
+      }
+    }
+    &-context {
+      margin-top: 8px;
+      font-size: 14px;
+    }
+    &-in {
+      margin: 24.68px auto 0;
+      padding: 0 21.5px;
+      display: grid;
+      // flex-wrap: wrap;
+      grid-template-columns: repeat(2, 1fr);
+      gap: 15px 0px;
+      // justify-content: center;
+      padding: 35px 0;
+      box-sizing: border-box;
+      justify-items: center;
+      & > div,
+      & > a {
+        // flex: initial;
+        // width: calc(100% / 3);
+        // padding: 0 8.5px 10px;
+        gap: 0 5px;
+        padding: 0;
+        & > div:nth-child(1) {
+          font-size: 8px;
+          width: 17.94vw;
+          height: 17.94vw;
+          & > img {
+            width: 100%;
+          }
+        }
+        & > div:nth-child(2) {
+          min-width: 19.23vw;
+          display: flex;
+          text-align: left;
+          font-weight: 600;
+          line-height: 130%; /* 12.12px */
+          font-size: 2.3vw;
+          padding: 0;
+          flex-direction: column;
+          letter-spacing: 0;
+          & > span {
+            display: inline;
+            color: var(--Grey-Deep, #4d4d4d);
+            font-family: FakePearl;
+            font-style: normal;
+          }
+        }
+      }
+    }
+  }
+  .honor-box {
+    margin-top: 60px;
+    margin-bottom: 0;
+    opacity: 0.8;
+    background: linear-gradient(
+      0deg,
+      rgba(255, 241, 240, 0) 0.89%,
+      rgba(255, 241, 240, 0.7) 22.95%,
+      rgba(255, 241, 240, 0.7) 76.25%,
+      rgba(255, 241, 240, 0) 99.11%
+    );
   }
 }
 </style>
